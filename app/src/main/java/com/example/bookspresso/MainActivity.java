a43b10c2816b8b18f5b4494b61fe467db0eacd77
ruhsulin <alinaspahiu.a@gmail.com>
2025-02-05 17:17:40 +0100

    Updated MainActivity to use switch between tabs in bottom nav bar.

--- app/src/main/java/com/example/bookspresso/MainActivity.java
+++ app/src/main/java/com/example/bookspresso/MainActivity.java
@@ -1,12 +1,52 @@
 package com.example.bookspresso;
 
-import android.app.Activity;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import android.view.MenuItem;
+import com.example.bookspresso.HomeFragment;
+import com.example.bookspresso.AddBookFragment;
+import com.example.bookspresso.GoalFragment;
 
-public class MainActivity extends Activity {
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(navListener);
+
+        // Load Home Fragment by default
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new HomeFragment())
+                    .commit();
+        }
     }
+
+    private final BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+
+                    if (item.getItemId() == R.id.nav_home) {
+                        selectedFragment = new HomeFragment();
+                    } else if (item.getItemId() == R.id.nav_add_book) {
+                        selectedFragment = new AddBookFragment();
+                    } else if (item.getItemId() == R.id.nav_goal) {
+                        selectedFragment = new GoalFragment();
+                    }
+
+                    if (selectedFragment != null) {
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container, selectedFragment)
+                                .commit();
+                    }
+                    return true;
+                }
+            };
 }
