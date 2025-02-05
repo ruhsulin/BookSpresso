386412a3b6a7e4fa2734a7437fe67cf40ca896ff
ruhsulin <alinaspahiu.a@gmail.com>
2025-02-05 17:12:07 +0100

    Added AddBook Fragment

---
+++ app/src/main/java/com/example/bookspresso/AddBookFragment.java
@@ -0,0 +1,16 @@
+package com.example.bookspresso;
+
+import android.os.Bundle;
+import androidx.fragment.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import com.example.bookspresso.R;
+
+public class AddBookFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_add_book, container, false);
+    }
+}
