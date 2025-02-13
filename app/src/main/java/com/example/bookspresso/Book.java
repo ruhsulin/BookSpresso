b74da7c779bc39e2ec1250ecf2634d9efbc15e94
ruhsulin <alinaspahiu.a@gmail.com>
2025-02-14 00:39:38 +0100

    Refactored Book class to implement Serializable for intent passing.

--- app/src/main/java/com/example/bookspresso/Book.java
+++ app/src/main/java/com/example/bookspresso/Book.java
@@ -1,6 +1,8 @@
 package com.example.bookspresso;
 
-public class Book {
+import java.io.Serializable;
+
+public class Book implements Serializable {
     private int id;
     private String title;
     private String author;
