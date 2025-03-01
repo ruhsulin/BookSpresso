9ff3f32aab4ae9c01f2f42ef3ca73573dc7f05f4
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-01 15:59:44 +0100

    Added onResume method to not show the addBook button in GoalsFragment.

--- app/src/main/java/com/example/bookspresso/GoalFragment.java
+++ app/src/main/java/com/example/bookspresso/GoalFragment.java
@@ -5,10 +5,25 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class GoalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_goal, container, false);
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        // Hide Floating Add Book button in Goals.
+        FloatingActionButton fab = getActivity().findViewById(R.id.fabAddBook);
+        if (fab != null) {
+            fab.hide();
+        }
+    }
+
 }
