83c2633b2239a19a4f09d821d76ee1fff4fe259d
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-05 22:19:18 +0100

    Refactor: Added lamda exprstion forclean coding Added validateInput() method to validate email and password, Added checkUserSession() Added saveUserSession() Added saveUserEmail() method added onDestroy() method to prevent memory leak.

--- app/src/main/java/com/example/bookspresso/activities/LoginActivity.java
+++ app/src/main/java/com/example/bookspresso/activities/LoginActivity.java
@@ -1,6 +1,5 @@
 package com.example.bookspresso.activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
@@ -13,18 +12,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.example.bookspresso.R;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
+import com.example.bookspresso.utils.ToastHelper;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
     private EditText etEmail, etPassword;
-    private Button btnLogin;
-    private TextView tvRegisterHere;
     private ProgressBar progressBar;
     private FirebaseAuth auth;
 
@@ -35,112 +29,111 @@ public class LoginActivity extends AppCompatActivity {
 
         // Initializing Firebase
         auth = FirebaseAuth.getInstance();
+
         //mapping UI elements
         etEmail = findViewById(R.id.etEmail);
         etPassword = findViewById(R.id.etPassword);
-        btnLogin = findViewById(R.id.btnLogin);
-        tvRegisterHere = findViewById(R.id.tvAlreadyHaveAccount);
+        Button btnLogin = findViewById(R.id.btnLogin);
+        TextView tvRegisterHere = findViewById(R.id.tvAlreadyHaveAccount);
         progressBar = findViewById(R.id.progressBar);
 
-        SharedPreferences preferences = getSharedPreferences("BookSpresso", Context.MODE_PRIVATE);
-        String savedEmail = preferences.getString("Email", "");
+        // Check if user is Logged in
+        checkUserSession();
+
+        // Click Listeners
+        //login
+        btnLogin.setOnClickListener(view -> loginUser());
+
+        // register
+        tvRegisterHere.setOnClickListener(view -> startActivity(new Intent(LoginActivity.this, RegisterActivity.class)));
+    }
+
+    // loginUser method
+    public void loginUser() {
+        progressBar.setVisibility(View.VISIBLE);
+
+        String email = etEmail.getText().toString();
+        String password = etPassword.getText().toString();
+
+        //Validating email and password
+        if (!validateInput(email, password)) {
+            progressBar.setVisibility(View.GONE);
+            return;
+        }
+
+        //sign in with Firebase
+        auth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, task -> {
+                    progressBar.setVisibility(View.GONE);
+                    if (task.isSuccessful()) {
+                        ToastHelper.showToast(LoginActivity.this, "Login Successful!");
+
+                        // Save User Session
+                        String userId = auth.getCurrentUser().getUid();
+                        saveUserSession(userId);
+                        saveUserEmail(email);
+
+                        // Navigate to MainActivity
+                        Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        ToastHelper.showToast(LoginActivity.this, "Login Failed: " + task.getException().getMessage());
+                    }
+                });
+    }
+
+    // checkUserSession
+    private void checkUserSession() {
+        SharedPreferences sharedPreferences = getSharedPreferences("BookSpresso", Context.MODE_PRIVATE);
+        String savedEmail = sharedPreferences.getString("Email", "");
 
         if (!TextUtils.isEmpty(savedEmail)) {
             Intent intent = new Intent(this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        }
-
-        savedEmail = ReadSharedPreferences();
-        if (!TextUtils.isEmpty(savedEmail)) {
-            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
             intent.putExtra("username", savedEmail);
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
         }
-
-        btnLogin.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view) {
-                loginUser();
-            }
-        });
-
-        tvRegisterHere.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
-                startActivity(intent);
-            }
-        });
-
-    }
-    // loginUser method
-    public void loginUser(){
-        String email = etEmail.getText().toString();
-        String password = etPassword.getText().toString();
-
-        //Validating email and password
-        if (TextUtils.isEmpty(email)) {
-            showToast("Please enter your email");
-            return;
-        }
-        if(TextUtils.isEmpty(password)){
-            showToast("Please enter your password");
-            return;
-        }
-
-        progressBar.setVisibility((View.VISIBLE));
-
-        //sign user with Firebase
-        auth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-                        progressBar.setVisibility(View.GONE);
-
-                        if(task.isSuccessful()){
-                            showToast("Login Successful!");
-
-                            //Saving userId in SharedPReferences
-                            String userId = auth.getCurrentUser().getUid();
-                            saveUserSession(userId);
-
-                            Intent intent = new Intent(LoginActivity.this, MainActivity.class );
-                            WriteSharedPreferences(email);
-                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_CLEAR_TASK);
-                            startActivity(intent);
-                            finish();
-                        }
-                        else {
-                            showToast("Login Failed. Try Again! ");
-                        }
-                    }
-                });
     }
 
+    // saveUserSession
     private void saveUserSession(String userId) {
         SharedPreferences sharedPreferences = getSharedPreferences("UserSession", MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putString("userId", userId);
         editor.apply();
     }
-    // showText method
-    private void showToast(String message){
-        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
-    }
 
-    private void WriteSharedPreferences(String strEmail){
+    // saveUserEmail
+    private void saveUserEmail(String email) {
         SharedPreferences sharedPreferences = getSharedPreferences("BookSpresso", MODE_PRIVATE);
-
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putString("Email", strEmail);
+        editor.putString("Email", email);
         editor.apply();
     }
 
-    private String ReadSharedPreferences(){
-        SharedPreferences sharedPreferences = getSharedPreferences("BookSpresso", MODE_PRIVATE);
-        return sharedPreferences.getString("Email", "");
+    // Validating inputs
+    private boolean validateInput(String email, String password) {
+        if (TextUtils.isEmpty(email)) {
+            ToastHelper.showToast(this, "Please enter your email");
+            return false;
+        }
+        if (!android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            ToastHelper.showToast(this, "Enter a valid email address!");
+            return false;
+        }
+        if (TextUtils.isEmpty(password)) {
+            ToastHelper.showToast(this, "Please enter your password");
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        auth = null; // Prevent memory leaks
     }
 }
