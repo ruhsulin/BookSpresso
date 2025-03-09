4d6484e273a75f4513acfc66a502f9ce1f447bb0
ruhsulin <alinaspahiu.a@gmail.com>
2025-03-09 16:25:25 +0100

    Added Splach in Manifest.

---
+++ app/src/main/java/com/example/bookspresso/activities/SplashActivity.java
@@ -0,0 +1,33 @@
+package com.example.bookspresso;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.bookspresso.activities.MainActivity;
+
+public class SplashActivity extends AppCompatActivity {
+    private static final int SPLASH_TIME_OUT = 2000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView logo = findViewById(R.id.imageView);
+        Animation fadeIn = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        logo.startAnimation(fadeIn);
+
+        new Handler().postDelayed(() -> {
+            Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+        }, SPLASH_TIME_OUT);
+    }
+
+}
